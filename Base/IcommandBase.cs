3befc94ad2e9e88cf973d942ab10f0cb17981757
LAPTOP-OHJC2HCK\46432 <464325382@qq.com>
2022-09-06 13:33:46 +0800

    no message

---
+++ Base/IcommandBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DPM_Utility.Base
+{
+    internal class IcommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return DoCanExeccute?.Invoke(parameter) == true;
+        }
+
+        public void Execute(object parameter)
+        {
+            DoExeccute?.Invoke(parameter);
+        }
+
+        public Action<object> DoExeccute { get; set; }
+        public Func<object, bool> DoCanExeccute { get; set; }
+    }
+}
---
+++ INI.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace DPM_Utility
+{
+    public class INI
+    {
+
+        public string path;
+
+        public INI(string INIPath)
+        {
+            path = INIPath;
+        }
+        #region 其他
+        [DllImport("kernel32",CharSet =CharSet.Unicode)]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        private static extern int GetPrivateProfileString(string section, string key, string defVal, Byte[] retVal, int size, string filePath);
+        #endregion
+        [DllImport("kernel32", EntryPoint = "GetPrivateProfileString")]
+        private static extern uint GetPrivateProfileStringA(string section, string key,string def, Byte[] retVal, int size, string filePath);
+
+        public  List<string> ReadSections()
+        {
+            return ReadSections(path);
+        }
+
+        public  List<string> ReadSections(string iniFilename)
+        {
+            List<string> result = new List<string>();
+            Byte[] buf = new Byte[65536];
+            uint len = GetPrivateProfileStringA(null, null, null, buf, buf.Length, iniFilename);
+            int j = 0;
+            for (int i = 0; i < len; i++)
+                if (buf[i] == 0)
+                {
+                    result.Add(Encoding.Default.GetString(buf, j, i - j));
+                    j = i + 1;
+                }
+            return result;
+        }
+
+        public  List<string> ReadKeys(String SectionName)
+        {
+            return ReadKeys(SectionName, path);
+        }
+
+        public  List<string> ReadKeys(string SectionName, string iniFilename)
+        {
+            List<string> result = new List<string>();
+            Byte[] buf = new Byte[65536];
+            uint len = GetPrivateProfileStringA(SectionName, null, null, buf, buf.Length, iniFilename);
+            int j = 0;
+            for (int i = 0; i < len; i++)
+                if (buf[i] == 0)
+                {
+                    result.Add(Encoding.Default.GetString(buf, j, i - j));
+                    j = i + 1;
+                }
+            return result;
+        }
+
+        //写INI文件
+        /// <summary>
+        /// 写INI文件
+        /// </summary>
+        /// <param name="Section"></param>
+        /// <param name="Key"></param>
+        /// <param name="Value"></param>
+        public void IniWriteValue(string Section, string Key, string Value)
+        {
+            WritePrivateProfileString(Section, Key, Value, this.path);
+        }
+        //读取INI文件
+        /// <summary>
+        /// 读取INI文件
+        /// </summary>
+        /// <param name="Section"></param>
+        /// <param name="Key"></param>
+        /// <returns></returns>
+        public string IniReadValue(string Section, string Key)
+        {
+            StringBuilder temp = new StringBuilder(255);
+            int i = GetPrivateProfileString(Section, Key, "", temp, 255, this.path);
+            return temp.ToString();
+        }
+        public byte[] IniReadValues(string section, string key)
+        {
+            byte[] temp = new byte[255];
+            int i = GetPrivateProfileString(section, key, "", temp, 255, this.path);
+            return temp;
+        }
+        //删除Ini文件下所有段落
+        /// <summary>
+        /// 删除ini文件下所有段落
+        /// </summary>
+        public void ClearAllSection()
+        {
+            IniWriteValue(null, null, null);
+        }
+        //删除Ini文件下Personal段落下的所有键
+        /// <summary>
+        /// 删除Ini文件下Personal段落下的所有键
+        /// </summary>
+        /// <param name="Section"></param>
+        public void ClearSection(string Section)
+        {
+            IniWriteValue(Section, null, null);
+        }
+
+    }
+}
