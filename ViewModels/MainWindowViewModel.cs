ac8f92b376de58eb7e8328329be91b1ddaf25ae0
LAPTOP-OHJC2HCK\46432 <464325382@qq.com>
2022-09-06 14:40:21 +0800

    no message

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using DPM_Utility.Base;
+using DPM_Utility.Views;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace DPM_Utility.ViewModels
@@ -14,8 +16,20 @@ namespace DPM_Utility.ViewModels
         public ObservableCollection<string> ListButtonSource { get; set; } = new ObservableCollection<string>();
         private string[] ListButtonName = { "参数设定", "状态检测","软件配置" };
         private string[] ViewsList = { "DpmParameter","MotionDectection",""};
+
+        private FrameworkElement  _mainContent;
+
+        public FrameworkElement MainContent
+        {
+            get { return _mainContent; }
+            set { _mainContent = value; DoNotify(); }
+        }
+
+
         public MainWindowViewModel()
         {
+            //默认窗体就是软件设置界面窗体
+            MainContent = new SoftWareSetup();
             //加载列表
             for (int i = 0; i < ListButtonName.Length; i++)
             {
@@ -47,7 +61,20 @@ namespace DPM_Utility.ViewModels
 
         private void ShowDialog(int index)
         {
-                
+            switch (index)
+            {
+                case 0:
+                    MainContent = new SoftWareSetup();
+                    break;
+                case 1:
+                    MainContent = new DpmParameter();
+                    break;
+                case 2:
+                    MainContent = new MotionDetection();
+                    break;
+                default:
+                    break;
+            }
         }
 
 
